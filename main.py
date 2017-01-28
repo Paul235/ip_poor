139b8070bc75af9edb245ea8bcb7475ea4c6d802
Paul235 <1258035736@qq.com>
2017-01-28 23:01:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import time
 import os
 
 if __name__ == '__main__':
-    page = raw_input('输入尾页：\n')
+    page = raw_input('Input the end page number:\n')
     download_html.download(page)
     get_ip.alive_ip(page)
-
-
